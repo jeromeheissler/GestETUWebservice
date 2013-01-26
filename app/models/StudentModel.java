a96fa1b1f336acac7858cc01207ba2a87d1727e6
jerome <heissler.jerome@gmail.com>
2013-01-26 21:07:42 +0100

    commit

--- app/models/StudentModel.java
+++ app/models/StudentModel.java
@@ -22,6 +22,9 @@ public class StudentModel extends Model {
 	private String lastname;
 	private String numStu;
 	
+	private String email;
+	private String password;
+	
 	@Reference
 	private PromotionModel promotion;
 	@Reference
@@ -72,6 +75,22 @@ public class StudentModel extends Model {
 			}
 			nbBoucle--;
 		}
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
 	
